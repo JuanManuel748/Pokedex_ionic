cf0fc0fc73c67e3b339df75e3c26dc156d83d5f3
Juan Manuel Pérez <junperz163@gmail.com>
2025-02-10 11:07:30 +0100

    11 terminado pero no muestra la pagina

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, inject } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, UserCredential } from '@angular/fire/auth';
+import { User } from '../models/user.model';
+import { Firestore, setDoc, doc } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirebaseService {
+  auth = inject(Auth);
+  firestore = inject(Firestore);
+
+  signIn(user: User): Promise<UserCredential> {
+    return signInWithEmailAndPassword(
+      this.auth,
+      user.email,
+      user.password
+    );
+  }
+
+  signUp(user: User): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(
+      this.auth,
+      user.email,
+      user.password
+    );
+  }
+
+  async updateUser(displayName: string) {
+    const user = await this.auth.currentUser;
+    if (user) {
+      // Actualiza el perfil del usuario
+      await updateProfile(user, { displayName: displayName });
+    }
+  }
+}
