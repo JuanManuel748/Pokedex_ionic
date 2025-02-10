10fa5e61ddee915d3149335b6ce7039ac36822a2
Juan Manuel Pérez <junperz163@gmail.com>
2025-02-10 11:19:17 +0100

    Antes del paso 11, funciona

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,12 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, UserCredential } from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword, UserCredential } from '@angular/fire/auth';
 import { User } from '../models/user.model';
-import { Firestore, setDoc, doc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
   auth = inject(Auth);
-  firestore = inject(Firestore);
 
   signIn(user: User): Promise<UserCredential> {
     return signInWithEmailAndPassword(
@@ -16,21 +14,5 @@ export class FirebaseService {
       user.email,
       user.password
     );
-  }
-
-  signUp(user: User): Promise<UserCredential> {
-    return createUserWithEmailAndPassword(
-      this.auth,
-      user.email,
-      user.password
-    );
-  }
-
-  async updateUser(displayName: string) {
-    const user = await this.auth.currentUser;
-    if (user) {
-      // Actualiza el perfil del usuario
-      await updateProfile(user, { displayName: displayName });
-    }
   }
 }
