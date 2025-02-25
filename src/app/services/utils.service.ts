471445e270aca5d5ea13b179f5eeb7ee4f422792
Frenadol <fernandoramajimenez@gmail.com>
2025-02-25 10:21:01 +0100

    Todo echo antes del paso 22

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -7,6 +7,8 @@ import {
   ModalController,
   ModalOptions,
 } from '@ionic/angular/standalone';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -55,6 +57,22 @@ export class UtilsService {
 
   dismissModal(data?: any) {
     return this.modalController.dismiss(data);
+  }
+
+  async takePicture(promptLabelHeader: string) {
+    return await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Prompt,
+      promptLabelHeader,
+      promptLabelPhoto: 'Selecciona una imagen',
+      promptLabelPicture: 'Saca una foto'
+    });
+  }
+
+  getLocalStoredUser() : User | null {
+    return this.getFromLocalStorage('user');
   }
 
   constructor() {}
