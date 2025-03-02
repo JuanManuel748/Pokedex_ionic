1a77cdc427bbf59f04e5f24e10fe85d26a2de33a
Juan Manuel Pérez <junperz163@gmail.com>
2025-03-02 23:53:45 +0100

    Funcionalidad terminada

--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {emptyPokemon} from "../models/pokemon.model";
+import {emptyPokemon, Pokemon, Stats} from "../models/pokemon.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +8,6 @@ export class PokeapiService {
   baseUrl = 'https://pokeapi.co/api/v2/';
   spriteUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-  constructor(private http: HttpClient) { }
 
   async getAllPoke(page: number, size: number = 20): Promise<emptyPokemon[]> {
     if (page > 5) return [];
@@ -20,10 +18,21 @@ export class PokeapiService {
     return [];
   }
 
-  async getById(id: string): Promise<emptyPokemon> {
+  async getById(id: string): Promise<Pokemon> {
     const response = await fetch(`${this.baseUrl}pokemon/${id}`);
-    return await response.json();
+    let pokemon = await response.json();
+    pokemon.description = await this.getDescription(id);
+    return pokemon as Pokemon;
   }
+
+  async getDescription(id: string | number): Promise<string> {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    const resJson = await res.json();
+    const texto = resJson.flavor_text_entries.find((texto: any) => texto.language.name === "es");
+    return texto ? texto.flavor_text : "No se encontró descripción en español";
+  }
+
+
 
 
 
